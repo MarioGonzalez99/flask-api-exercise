469c9d701948bb8c1d5e6ab76521af5464813bd2
Mario Gonzalez <mariogonzalezd99@gmail.com>
2022-02-14 10:54:38 -0600

    :sparkles: Add auth validation

--- app/resources/users.py
+++ app/resources/users.py
@@ -1,3 +1,4 @@
+from email import message
 from app.database.model import db, User
 from flask_restful import Resource, abort, fields, marshal_with, reqparse
 
@@ -22,12 +23,29 @@ user_parser.add_argument('username', required=True,
 user_parser.add_argument('password', required=True,
                          help='Password required', type=non_empty_string)
 user_parser.add_argument('admin', type=check_boolean)
+user_parser.add_argument('Authentication', location='headers')
 
 
 def abort_if_username_already_exists(username):
     user = User.query.filter_by(username=username).first()
     if user:
         abort(409, message="A user with that username already exists.")
+
+
+def abort_if_creator_not_admin(token):
+    user_id = User.decode_auth_token(token)
+    try:
+        user_id = int(user_id)
+    except:
+        abort(401, message=user_id)
+
+    user = User.query.filter_by(id=user_id)
+    if user is None or user.admin == False:
+        abort(401, message='Not authorized to use this function')
+
+
+def check_if_admin(isAdmin):
+    return True if isAdmin is not None and isAdmin == "true" else False
 
 
 user_fields = {
@@ -46,11 +64,10 @@ class Users(Resource):
     @marshal_with(user_fields)
     def post(self):
         args = user_parser.parse_args()
+        abort_if_creator_not_admin(args['Authentication'])
         username = args['username']
         abort_if_username_already_exists(username)
-        isAdmin = False
-        if args['admin'] is not None and args['admin'] == "true":
-            isAdmin = True
+        isAdmin = check_if_admin(args['admin'])
         user = User(username=username, admin=isAdmin)
         user.hash_password(args['password'])
         db.session.add(user)
